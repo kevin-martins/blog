676d98c4d72f5cb2379f68813d0629fab580dfd3
Kevin <kev.martinsvieira@gmail.com>
2023-05-02 12:29:22 +0200

    feat: use Layout to fix header rendering

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { useEffect, useState } from 'react';
+import { Layout } from '../components';
+
+import '@/styles/globals.scss';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
