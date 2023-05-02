a5e264710e3337292ff0788b1def5dbd3ea35e33
Kevin <kev.martinsvieira@gmail.com>
2023-05-02 12:31:20 +0200

    feat: add next components that'll be updated afterwards

---
+++ components/PostWidget.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import moment from 'moment';
+import Link from 'next/link';
+
+const PostWidget = () => {
+  return (
+    <div>
+      PostWidget
+    </div>
+  )
+}
+
+export default PostWidget
