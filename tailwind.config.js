537c35b7773dab859d01f57491a9b1e28d61df42
Kevin <kev.martinsvieira@gmail.com>
2023-05-10 01:02:37 +0200

    fix: add lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './models/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './services/**/*.{js,ts,jsx,tsx,mdx}',
+    './helpers/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
